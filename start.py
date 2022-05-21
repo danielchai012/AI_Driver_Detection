21e5c4d1d50dda8cc2d793b192c5cc9cadec1ac3
danielchai012 <58755196+danielchai012@users.noreply.github.com>
2022-05-21 23:06:01 +0800

    Delete start.py

--- start.py
+++
@@ -1,8 +0,0 @@
-from PyQt5 import QtWidgets
-from controller import MainWindow_ctrl
-if __name__ == '__main__':
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    window = MainWindow_ctrl()
-    window.show()
-    sys.exit(app.exec_())
