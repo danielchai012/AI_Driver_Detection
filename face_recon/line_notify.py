7878f549d86ce6e407417114ccda3894ef01ac2c
danielchai012 <58755196+danielchai012@users.noreply.github.com>
2022-05-21 23:08:59 +0800

    Add files via upload

--- face_recon/line_notify.py
+++ face_recon/line_notify.py
@@ -14,6 +14,20 @@ def lineNotifyMessage(msg):
     return r.status_code
 
 
+def lineNotifyMessage_Pic(msg, img_path):
+    token = 'X8TD43e49jlLWDMFGDLD3ATshAHSM3tn4h1SDU0O2Bg'
+    headers = {
+        "Authorization": "Bearer " + token
+    }
+    print(img_path)
+    data = {'message': msg
+            }
+    image = open(img_path, 'rb')
+    imageFile = {'imageFile': image}
+    r = requests.post("https://notify-api.line.me/api/notify",
+                      headers=headers, data=data, files=imageFile)
+    return r.status_code
+
 # if __name__ == "__main__":
 #     token = 'X8TD43e49jlLWDMFGDLD3ATshAHSM3tn4h1SDU0O2Bg'
 #     message = '基本功能測試'
